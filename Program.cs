6dbcbb615f0aa10fa89fba886002e16344b4f1c3
LucSan10 <santiagolucas643@gmail.com>
2023-03-11 23:10:29 -0300

    adjustments

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using Fipe.Modelos;
 
 HttpClient client = new();
@@ -8,16 +10,18 @@ const string tabelaReferenciaURL = "https://brasilapi.com.br/api/fipe/tabelas/v1
 const string fipeURL = "https://veiculos.fipe.org.br/api/veiculos";
 
 var tabelasReferencia = await client.GetFromJsonAsync<List<TabelaReferencia>>(tabelaReferenciaURL);
+var primeiraTabelaRef = tabelasReferencia![0].Codigo.ToString();
+
 
 var payload = new Dictionary<string, string>
 {
-    { "codigoTabelaReferencia", tabelasReferencia![0].Codigo.ToString() },
+    { "codigoTabelaReferencia", primeiraTabelaRef },
     { "codigoTipoVeiculo", "1" }
 };
 
 var body = new FormUrlEncodedContent(payload);
 
-var response = await client.PostAsync(fipeURL + "/ConsultarMarcas", body);
+var response = await client.PostAsync($"{fipeURL}/ConsultarMarcas", body);
 var marcas = await response.Content.ReadFromJsonAsync<List<Marca>>();
 var modelosMarcas = new List<ModelosMarca>();
 
@@ -25,25 +29,28 @@ foreach (Marca marca in marcas!)
 {
     payload = new Dictionary<string, string>
     {
-        { "codigoTabelaReferencia", tabelasReferencia![0].Codigo.ToString() },
+        { "codigoTabelaReferencia", primeiraTabelaRef },
         { "codigoTipoVeiculo", "1" },
         { "codigoMarca", marca.Codigo }
     };
 
     body = new FormUrlEncodedContent(payload);
 
-    response = await client.PostAsync(fipeURL + "/ConsultarModelos", body);
+    response = await client.PostAsync($"{fipeURL}/ConsultarModelos", body);
     var anoModelos = await response.Content.ReadFromJsonAsync<AnoModelo>();
 
     var modelosMarca = new ModelosMarca { Marca = marca, Modelos = anoModelos!.Modelos };
     modelosMarcas.Add(modelosMarca);
 }
 
-var count = 0;
-foreach (ModelosMarca mm in modelosMarcas)
+var options = new JsonSerializerOptions
 {
-    count += mm.Modelos.Count;
-    Console.WriteLine($"{mm.Marca.Nome}: {mm.Modelos.Count}");
+    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+    WriteIndented = true
+};
 
-    Console.WriteLine($"Total: {count}\n");
-}
+modelosMarcas = modelosMarcas.OrderByDescending(mm => mm.Modelos.Count).ToList();
+modelosMarcas.ForEach(mm => Console.WriteLine($"{mm.Marca.Nome}: {mm.Modelos.Count}\n"));
+
+var total = modelosMarcas.Aggregate(0, (acc, mm) => acc + mm.Modelos.Count);
+Console.WriteLine($"total: {total}");
